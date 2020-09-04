25d32ed9787080fa0b0246f5be8be029ffa1c060
satish <mnsvp11@gmail.com>
2020-09-04 11:56:23 +0530

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? welcome to devOps Project')
+  response.send('Hello Asma! How are you?')
 }) 
 
 app.listen(app.get('port'), function() {
